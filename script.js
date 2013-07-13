ed58f434b74fd01521cfde4e671a6cd519d03b39
Ryan Dumlao <dlogsr@gmail.com>
2013-07-13 02:13:16 -0700

    changed bar layout

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ $(document).ready(function(){
 	var $navBar = $('#navBar');
 	var $nameBarH1 = $('#nameBar h1');
 	var $nameBarH2 = $('#nameBar h2');
+	var $paragraph = $('#paragraph');
 	
 	$navCat.click(function() {
 		$mainBar.animate({'bottom':0,
-					      'width':'100%',
+					      'width':'600px',
 						  'left':0,
 						  'margin-left':0},1000);
   		$nameBarH1.animate({'font-size':'3em'},1000);
 		$nameBarH2.fadeOut(400);
-		$nameBar.add($navBar).animate({'height':50,'border-bottom-right-radius':0},1000);
+		$navBar.animate({'border-top-right-radius':'25px'},100);
+		$navBar.add($nameBar).animate({'height':'50px',
+									   'border-bottom-right-radius':0,
+									   'border-top-left-radius':0,},1000);
 		$('#nameBarPrimary').animate({'top':'-40px'},1000);
 		$mainBar.removeClass('absolutePos').addClass('fixedPos');
+		$paragraph.removeClass('hidden');
 
 	});
 });// JavaScript Document
